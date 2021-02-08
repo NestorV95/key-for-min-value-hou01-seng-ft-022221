f8d46790e5773492510377a590a37e0922cc100d
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-08 06:01:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 min_value = 999999999999999
 def key_for_min_value(hash)
 hash.each do |key , value|
-if key > min_value
-  min_value = key
+if value > min_value
+  min_value = value
   min_value
     
   end
