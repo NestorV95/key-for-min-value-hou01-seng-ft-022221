5cbe48bf3bea3be39d71f8c48c377ccbb6807df5
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-08 06:02:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-min_value = 999999999999999
+
 def key_for_min_value(hash)
   
 hash.each do |key , value|
